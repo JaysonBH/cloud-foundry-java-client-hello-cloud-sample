8699171a58b7f80cf827d8a49c212c5d34c97b29
JaysonBH <jhairston@pivotal.io>
2020-01-15 11:41:17 -0500

    Add Hello Cloud Example Output of the /info endpoint of the api

--- src/main/java/com/vmware/tanzu/support/cfclienttaskdemo/CfClientTaskDemoApplication.java
+++ src/main/java/com/vmware/tanzu/support/cfclienttaskdemo/CfClientTaskDemoApplication.java
@@ -1,5 +1,10 @@
 package com.vmware.tanzu.support.cfclienttaskdemo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.client.v2.info.GetInfoRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,13 +14,21 @@ import org.springframework.cloud.task.configuration.EnableTask;
 @SpringBootApplication
 public class CfClientTaskDemoApplication implements CommandLineRunner {
 
+	private static final Log LOG = LogFactory.getLog(CfClientTaskDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CfClientTaskDemoApplication.class, args);
 	}
 
+	@Autowired
+	private CloudFoundryClient client;
+
 	@Override
 	public void run(String... args) {
-		System.out.println("Hello, World!");
+
+		//Shows information similar to `cf curl /` and `cf curl /info`
+		LOG.info( client.info().get(GetInfoRequest.builder().build()).block() );
+
 	}
 
 }
